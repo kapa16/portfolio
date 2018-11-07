aedba68fd1e728fa4e93156faedb3ac47e601895
Pavel <kapa16@inbox.ru>
2018-11-07 15:35:16 +0500

    do

--- dist/js/main.js
+++ dist/js/main.js
@@ -8,4 +8,20 @@ $(document).ready(function() {
             $(".sticky").removeClass("sticky_fixed");
     });
 
+    let arrPercents = [92, 80, 24, 16];
+
+    for(let i=0; i<arrPercents.length; i++) {
+        let canvas = document.getElementById("value"+(i+1)),
+            context = canvas.getContext("2d");
+        context.arc(70, 70, 65, -Math.PI / 2, (Math.PI * 2) / 100 * arrPercents[i] - Math.PI / 2, false)
+        context.strokeStyle = "#ff5252";
+        context.lineWidth = 10;
+        context.stroke();
+
+        context.fillStyle = "#666";
+        context.font = "bold 20pt Arial";
+        context.textAlign = "center";
+        context.textBaseline = "middle";
+        context.fillText(arrPercents[i]+"%", 70, 70);
+    }
 });
